faa44c93258ec024b2110d8d68b9a38fc139c58b
Murad J <agariomadman@gmail.com>
2021-07-08 01:25:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 // SETTINGS
 var port = process.env.PORT || 3000
-var password = ""; // No password by default
 
 const express = require('express');
 
 const app = express()
 const server = app.listen(port, () => console.log(`server listening on port: ${port}`))
 
-var sentPassword;
 const io = require('socket.io')(server)
 io.on('connection', (socket) => {
-    if(password != "" && sentPassword != password){
-    io.to(socket.id).emit('sentPassword', true)
-    socket.on('sentPassword', function(pass){
-        if(password == pass){
-            io.to(socket.id).emit('authenticated', true)
-        } else{
-            io.to(socket.id).emit('authenticated', false)
-        }
-    })
-} else{
-    socket.to(socket.id).emit('sentPassword', false)
-}
-    if(sentPassword == password || password == ""){
     console.log('New connection!')
     socket.on('mouseMovement', function(data){
         socket.broadcast.emit('mouseMovement', data)
@@ -36,5 +21,4 @@ io.on('connection', (socket) => {
         console.log('disconnected')
     })
     socket.broadcast.emit('online')
-}
 })
