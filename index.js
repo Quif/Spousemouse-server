aa8506f5c8801e318284d28e8c7dbf16cb85c05c
Murad J <agariomadman@gmail.com>
2021-07-07 20:04:33 +0400

    main

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+var port = process.env.PORT || 3000
+const app = express()
+const server = app.listen(port, () => console.log(`server listening on port: ${port}`))
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+    console.log('New connection!')
+    socket.on('mouseMovement', function(data){
+        socket.broadcast.emit('mouseMovement', data)
+        socket.broadcast.emit('movementAlert')
+        console.log("New mouse movement!")
+    });
+    socket.on('wave', function(){
+        socket.broadcast.emit('wave')
+    })
+    socket.on('disconnected', (evt) => {
+        console.log('disconnected')
+    })
+    socket.broadcast.emit('online')
+})
